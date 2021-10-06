e041168d19bdf38db6075ec5cb4d3b6b92ff7241
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-10-06 21:56:26 +0300

    initial commit

---
+++ src/main/java/ru/netology/Item.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Item {
+    private int id;
+    private String name;
+    private String genre;
+    private String imageUrl;
+
+}
